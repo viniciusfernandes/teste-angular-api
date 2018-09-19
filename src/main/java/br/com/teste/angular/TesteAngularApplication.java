c85d117bc44aa3e4f9ace07fbdbd2e35620cea17
vinicius <viniciussf@hotmail.com>
2018-09-19 15:46:42 -0300

    Criacao do projeto

---
+++ src/main/java/br/com/teste/angular/TesteAngularApplication.java
@@ -0,0 +1,12 @@
+package br.com.teste.angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteAngularApplication.class, args);
+	}
+}
